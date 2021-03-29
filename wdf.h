09aa3e02adbf064b48de56250b0f919b006d73db
Samuel Schachter <sschach2@UR.Rochester.edu>
2021-03-29 18:13:59 -0400

    Added port, tree, node and resistor defs

--- wdf.h
+++ wdf.h
@@ -3,6 +3,19 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+class wdfTree //a special instance of this class is created for each circuit to be run
+{
+public:
+	wdfTree() {}
+	virtual ~wdfTree() {}
+
+	virtual float processSample(float inSamp) = 0;
+	float setSampleRate(float SR) { sr = SR; }
+	float setAlpha(float a) { alpha = a; }
+private:
+	float sr;
+	float alpha;
+};
 class wdfPort
 {
 protected:
@@ -13,11 +26,11 @@ public:
 	wdfPort(std::string portType) : portType(portType) {}
 	virtual ~wdfPort() {}
 
-	virtual void calculateImpedance() = 0;
-	virtual void propagateImpedance() = 0;
+	virtual void calculateImpedance() = 0; //calculates the port impedance
+	virtual void propagateImpedance() = 0; //propagate port impedance up the tree
 
-	virtual void calculateIncidentWave(float x) = 0;
-	virtual void calculateReflectedWave() = 0;
+	virtual void calculateIncidentWave(float x) = 0; //find the wave leaving the port
+	virtual void calculateReflectedWave() = 0; //find the wave entering the port
 
 	float getPortVoltage()
 	{
@@ -30,4 +43,39 @@ public:
 
 private:
 	std::string portType;
+};
+
+class wdfNode : public wdfPort
+{
+public:
+	wdfNode(std::string type) : wdfPort(type) {}
+	virtual ~wdfNode() {}
+
+	void connectToNode(wdfPort* node) { connectedNode = node; } //connect ports together
+
+	void propagateImpedance() override
+	{
+		calcImpedance();
+
+		if (connectedNode != nullptr)
+			connectedNode->propagateImpedance();
+	}
+
+protected:
+	wdfPort* connectedNode;
+
+};
+
+class Resistor : public wdfNode
+{
+public:
+	Resistor(float R) : wdfNode("Resistor"), R_val(R)
+	{
+		calcImpedance();
+	}
+
+	~Resistor() {}
+
+private:
+	R_val = 0.1;
 };
